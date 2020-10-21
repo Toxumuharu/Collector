ad159b08a96f88438c5dc9e2284eaa192dd33404
Toxumuharu <ring.o.awlright@gmail.com>
2020-10-22 03:00:54 +0900

    UI design

---
+++ Collector/CreateCollectableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CreateCollectableViewController.swift
+//  Collector
+//
+//  Created by Toxumuharu on 2020/10/22.
+//
+
+import UIKit
+
+class CreateCollectableViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func mediaFolderTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
