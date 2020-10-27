f1a34af60d1d4ef3c645ff9ea513a8aad55829e4
Toxumuharu <ring.o.awlright@gmail.com>
2020-10-28 00:19:14 +0900

    set contents to tableView and use Core Data

--- Collector/CreateCollectableViewController.swift
+++ Collector/CreateCollectableViewController.swift
@@ -7,21 +7,41 @@
 
 import UIKit
 
-class CreateCollectableViewController: UIViewController {
+class CreateCollectableViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     
+    var pickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        pickerController.delegate = self
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage{
+            imageView.image = image
+        }
+        pickerController.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func mediaFolderTapped(_ sender: Any) {
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
     }
     @IBAction func cameraTapped(_ sender: Any) {
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
     }
     @IBAction func addTapped(_ sender: Any) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext{
+            let collectable = Collectable(context: context)
+            collectable.title = titleTextField.text
+            collectable.image = imageView.image?.jpegData(compressionQuality: 1.0)
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        }
+        navigationController?.popViewController(animated: true)
     }
 }
